c8c972ecbd00de4c25b52c1e93bfac8352df24d5
黃鼎文 <aa6413779@hotmail.com.tw>
2023-07-16 22:57:39 +0800

    API URL change

--- src/services/auth.service.js
+++ src/services/auth.service.js
@@ -1,5 +1,5 @@
 import axios from "axios";
-const API_URL = "http://localhost:8080/api/user";
+const API_URL = "https://mern-practice-server.onrender.com/api/user";
 
 class AuthService {
   login(email, password) {
